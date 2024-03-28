922db80f6d003633dfed109e491b5cc10529eb5f
poli <xyz@gmail.com>
2024-03-28 08:37:45 +0530

    Convert celsius to Fohrenheit

---
+++ JavaScript.js
@@ -0,0 +1,7 @@
+ // Celsius to fohrenheit
+let celsius = parseInt(prompt("Enter Celsius"));
+let fohren = parseInt(prompt("Enter fohren value"));
+let fohrenheit = celsius*1.8 + 32;
+let cel = (fohren - 32)/1.8;
+document.write(`The value in fohrenhit is :${fohrenheit} <br>`);
+document.write(`The value in fohrenhit is :${cel}`);
